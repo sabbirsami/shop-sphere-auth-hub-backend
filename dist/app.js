cb79e0bc62d0bb8a54ca62bbdaeef5a219a21fdf
sabbirsami <smd71430@gmail.com>
2025-06-01 23:01:48 +0600

    feat: backend functionality done

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
+// application routes
+app.use('/api/', routes_1.default);
 app.get('/', (req, res) => {
     res.send('Working');
 });
+app.use(globalErrorHandler_1.default);
 // Not Found
 app.all('*', (req, res, next) => {
     const error = new Error(`Can't find ${req.originalUrl} route on the server`);
